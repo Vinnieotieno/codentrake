4fa04d25f025f56b7ae7941a9b7b0f485e7d47c6
Rollins <rshadrackochieng@gmail.com>
2024-03-08 13:20:06 +0300

    changes

--- src/pages/LandingPage/sections/We-do.jsx
+++ src/pages/LandingPage/sections/We-do.jsx
@@ -8,28 +8,29 @@ import Container from '@/components/Container'
 const Wedo = () => {
   return (
     <Container className="w-full max-w-[1600px] bg-brandLighter bg-opacity-40 text-brandDark py-20 ">
-      <div className="md:flex justify-between items-center mb-7">
+      <div className="md:flex justify-between items-center mb-3">
         <span className="font-bold text-2xl capitalize">what we do</span>
         <Link to={"/services"} className="flex items-center">
           <span className="mr-2">Explore all services</span>
           <ArrowRight />
         </Link>
       </div>
+      <div className="flex flex-col space-y-2 mb-5">
+        <h2 className="text-md font-semibold">Perfect Solution For Your Business</h2>
+        <span className="text-sm leading-[20px]">
+          Discover our range of software products engineered to transcend mere aesthetics. We specialize in crafting purposeful experiences that align
+          with your brand's vision and goals.
+        </span>
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <div className="flex flex-col space-y-3">
-          <div className="flex flex-col space-y-2">
-            <h2 className="text-md font-semibold">Perfect Solution For Your Business</h2>
-            <span className="text-sm leading-[20px]">
-              Discover our range of software products engineered to transcend mere aesthetics. We specialize in crafting purposeful experiences that
-              align with your brand's vision and goals.
-            </span>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             {services.map((service, idx) => (
               <Link to={service.to} key={idx}>
-                <div className="sm:w-[300px] shadow-xl p-5 rounded-lg mt-2 bg-white">
+                <div className="shadow-xl p-5 rounded-lg mt-2 bg-white">
                   <h2 className="font-semibold py-2 flex justify-center">{service.title}</h2>
-                  <p className="text-gray-800">{service.description}</p>
+                  <p className="text-gray-800 text-xs">{service.description}</p>
                 </div>
               </Link>
             ))}
